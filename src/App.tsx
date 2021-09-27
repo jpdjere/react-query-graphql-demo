405d509933974bbed2cde81685a33e8ccffd9372
jpdjere <jpdjeredjian@gmail.com>
2021-09-27 09:52:42 +0200

    Set up app

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { QueryClientProvider, QueryClient } from 'react-query';
 
-function App() {
+import './App.css';
+import { CreateUser } from './CreateUser';
+import { UserList } from "./UserList"
+
+const queryClient = new QueryClient();
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <header className="App-header">
+          <h1>React Query Demo</h1>
+        </header>
+        <UserList />
+				<CreateUser />
+      </div>
+    </QueryClientProvider>
   );
 }
 
